e8fec2cbe46a05d8205a3805e254d34ce7219dd5
DongChyeon <kimdonghyun90766@gmail.com>
2023-03-18 16:14:11 +0900

    Release : FieldMate 1.0.0

--- app/src/main/java/com/hana/fieldmate/MainActivity.kt
+++ app/src/main/java/com/hana/fieldmate/MainActivity.kt
@@ -3,14 +3,12 @@ package com.hana.fieldmate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hana.fieldmate.ui.theme.FieldMateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
